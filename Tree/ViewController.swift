7db556c06b0f3334e60faeade6473afdea3c74bc
BoMin <lbmleebm@gmail.com>
2024-01-10 12:00:54 +0900

    FEAT: Initial Commit
    Project Setup
    - Add .gitignore
    - Create Podfile

---
+++ Tree/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Tree
+//
+//  Created by BoMin Lee on 1/10/24.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
