a99184055c475d46381d19699607d7f7ce429a85
BoMin <lbmleebm@gmail.com>
2024-01-10 15:10:41 +0900

    CHORE: Setup Again

--- Tree/ViewController.swift
+++ Tree/ViewController.swift
@@ -5,20 +5,13 @@
 //  Created by BoMin Lee on 1/10/24.
 //
 
-import Cocoa
+import UIKit
 
-class ViewController: NSViewController {
+class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
-    }
-
-    override var representedObject: Any? {
-        didSet {
-        // Update the view, if already loaded.
-        }
     }
 
 
