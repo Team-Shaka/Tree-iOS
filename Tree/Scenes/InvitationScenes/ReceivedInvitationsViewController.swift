06b9e9462f9b6cfda8c557169351cdbfaa3decad
BoMin <lbmleebm@gmail.com>
2024-01-19 16:14:14 +0900

    FEAT: ReceivedInvitationsViewController

---
+++ Tree/Scenes/InvitationScenes/ReceivedInvitationsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ReceivedInvitationsViewController.swift
+//  Tree
+//
+//  Created by BoMin Lee on 1/13/24.
+//
+
+import UIKit
+
+class ReceivedInvitationsViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configure()
+        addSubviews()
+        makeConstraint()
+    }
+    
+    private func configure() {
+        
+    }
+    
+    private func addSubviews() {
+        
+    }
+    
+    private func makeConstraint() {
+        
+    }
+}
