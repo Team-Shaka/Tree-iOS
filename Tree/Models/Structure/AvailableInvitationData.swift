f56ea2d11f78377da5fbf958bcc0f6d6f1022653
BoMin <lbmleebm@gmail.com>
2024-01-20 14:49:40 +0900

    FEAT: Create AvailableInvitationData

---
+++ Tree/Models/Structure/AvailableInvitationData.swift
@@ -0,0 +1,13 @@
+//
+//  AvailableInvitationData.swift
+//  Tree
+//
+//  Created by BoMin Lee on 1/20/24.
+//
+
+import Foundation
+
+struct AvailableInvitationData: Codable {
+    let availableInvitation: Int
+    let activeRate: Int
+}
