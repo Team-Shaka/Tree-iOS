1dfb627348e551a3c54902a69d9c3f6fea295de3
BoMin <lbmleebm@gmail.com>
2024-01-20 15:34:39 +0900

    FEAT: Add to StringCollections

--- Tree/Utils/TreeCollections/TreeStringCollection.swift
+++ Tree/Utils/TreeCollections/TreeStringCollection.swift
@@ -6,3 +6,51 @@
 //
 
 import Foundation
+
+protocol EnumeratedLocalized {
+    var rawValue: String { get }
+}
+
+extension EnumeratedLocalized {
+    var localized: String {
+        NSLocalizedString(self.rawValue, comment: "")
+    }
+}
+
+final class TreeStringCollection {
+    private init() { }
+    
+    static let appName: String = NSLocalizedString("appName", comment: "")
+    static let appVersion = Bundle.main.infoDictionary!["CFBundleShortVersionString"] as! String
+    
+    enum OnBoarding: String, EnumeratedLocalized {
+        case greetings
+        case phoneEnter
+        case phonePlaceholder
+        case phoneDescription
+        case sendVerification
+        case enterVerification
+        case verify
+        case hello
+        case getUserId
+        case userIdDescription
+        case userIdPlaceholder
+        case seeMyProfile
+        case oh
+        case whatANice
+        case youWillBeSeen
+        case memberNameDescription
+        case noIwantToChange
+        case likeIt
+        case congratulations
+        case seeInvitations
+        case firstInvitationsDescription
+    }
+    
+    enum Button: String, EnumeratedLocalized {
+        case see
+        case deny
+    }
+    
+    
+}
