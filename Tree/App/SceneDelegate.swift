fed8fc2f56bd7846bfcbbddbe236fbb53a6ba0f5
BoMin <lbmleebm@gmail.com>
2024-01-30 14:36:05 +0900

    FEAT: Update SceneDelegate

--- Tree/App/SceneDelegate.swift
+++ Tree/App/SceneDelegate.swift
@@ -16,7 +16,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(frame: windowScene.coordinateSpace.bounds)
         window?.windowScene = windowScene
-        window?.rootViewController = UINavigationController(rootViewController: FeedViewController())
+        window?.rootViewController = UINavigationController(rootViewController: PhoneNumberVerifyViewController())
+//        window?.rootViewController = PhoneNumberInsertViewController()
         window?.makeKeyAndVisible()
         guard let _ = (scene as? UIWindowScene) else { return }
     }
